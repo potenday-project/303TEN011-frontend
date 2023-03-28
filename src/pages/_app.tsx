70e77cf5fca6ac5b5923cc1e03f574190621bc5a
devyouth94 <devyouth94@gmail.com>
2023-03-28 19:16:16 +0900

    Feat: 리액트 쿼리 프로바이더 설정 및 데브툴 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-if (typeof window === "undefined") {
-  (async () => {
-    const { server } = await import("@/mocks/server");
-    server.listen();
-  })();
-} else {
-  (async () => {
-    const { worker } = await import("@/mocks/browser");
-    worker.start();
-  })();
-}
+// if (typeof window === "undefined") {
+//   (async () => {
+//     const { server } = await import("@/mocks/server");
+//     server.listen();
+//   })();
+// } else {
+//   (async () => {
+//     const { worker } = await import("@/mocks/browser");
+//     worker.start();
+//   })();
+// }
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
