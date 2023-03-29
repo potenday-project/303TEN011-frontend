fae22ee359ae4a34132f0d0879eaa5b6a6b6aabc
devyouth94 <devyouth94@gmail.com>
2023-03-29 16:00:40 +0900

    Refactor: 아이콘 수정

--- src/static/icons.tsx
+++ src/static/icons.tsx
@@ -1,22 +1,56 @@
 interface Argument {
   color?: string;
   size?: number;
+  className?: string;
+  onClick?: () => void;
 }
 
-export const IconSearch = ({ color = "#f2f2f2" }: Argument) => {
+export const IconSearch = ({ className }: Argument) => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      className={className}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <g clipPath="url(#clip0_74_739)">
         <path
           d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
-          fill={color}
+          fill="currentColor"
         />
       </g>
       <defs>
         <clipPath id="clip0_74_739">
-          <rect width="24" height="24" fill={color} />
+          <rect width="24" height="24" fill="currentColor" />
         </clipPath>
       </defs>
+    </svg>
+  );
+};
+
+export const IconCancel = ({ color = "#111111", onClick }: Argument) => {
+  return (
+    <svg
+      onClick={onClick}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M19 1L1 19" stroke={color} strokeWidth="2" />
+      <path d="M19 19L1 0.999999" stroke={color} strokeWidth="2" />
+    </svg>
+  );
+};
+
+export const IconCheck = () => {
+  return (
+    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="11" cy="11" r="11" fill="white" />
+      <path d="M6 11.5L9.5 15L16.5 7" stroke="#2C2A29" strokeWidth="2" />
     </svg>
   );
 };
