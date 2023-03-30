95e5689a1a08606632621f9e0c41a51ae59f00fd
devyouth94 <devyouth94@gmail.com>
2023-03-31 00:20:48 +0900

    Refactor: css 수정

--- src/components/common/BottomButton.tsx
+++ src/components/common/BottomButton.tsx
@@ -7,7 +7,7 @@ const BottomButton = ({ text, onClick }: Props) => {
   return (
     <button
       onClick={onClick}
-      className="fixed bottom-0 left-0 z-20 flex h-[60px] w-full items-center justify-center rounded-t-[10px] bg-[#111] text-lg font-semibold text-white"
+      className="fixed bottom-0 left-0 z-20 flex h-[60px] w-full min-w-[320px] items-center justify-center rounded-t-[10px] bg-[#111] text-lg font-semibold text-white"
     >
       {text}
     </button>
