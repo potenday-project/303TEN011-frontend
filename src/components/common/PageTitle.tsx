6c17c4e5b0547731f9aa4c3a72621e17d14fb7dd
devyouth94 <devyouth94@gmail.com>
2023-03-30 21:40:11 +0900

    Feat: 로그인 페이지 구현

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -1,7 +1,7 @@
 interface Props {
   title: string;
   fontWeight?: string;
-  fontSize?: "text-xl" | "text-lg";
+  fontSize?: "text-xl" | "text-lg" | "text-2xl";
   fontColor?: "text-main-900" | "text-white";
   lineColor?: "bg-[#ddd]" | "bg-[#616161]";
   lineWeight?: "h-3" | "h-[10px]";
@@ -19,7 +19,9 @@ const PageTitle = ({
 }: Props) => {
   return (
     <div className={`${className} relative w-fit`}>
-      <h1 className={`relative z-10 block px-1 ${fontWeight} ${fontSize} ${fontColor}`}>{title}</h1>
+      <h1 className={`tex text relative z-10 block px-1 ${fontWeight} ${fontSize} ${fontColor}`}>
+        {title}
+      </h1>
       <div className={`absolute left-0 bottom-[-1px] ${lineWeight} w-full ${lineColor}`} />
     </div>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,50 @@
+import Container from "@/components/common/Container";
+import PageTitle from "@/components/common/PageTitle";
+import Image from "next/image";
+import LoginImage from "@/static/images/login.svg";
+import LoginBackgroundImage from "@/static/images/login_background.svg";
+
 const Login = () => {
   return (
     <>
-      <h1>로그인 페이지</h1>
+      <Container className="relative flex flex-col justify-between px-6 py-[30px]">
+        <div className="relative z-[2] flex flex-col items-start justify-start gap-[45px]">
+          <PageTitle title="하루 한줄" />
+
+          <div className="flex flex-col gap-1 text-2xl font-medium">
+            <div className="flex items-center gap-1">
+              <PageTitle title="오늘의 한줄" fontSize="text-2xl" />
+              <span>을</span>
+            </div>
+            <span>기록으로 남기는</span>
+          </div>
+        </div>
+
+        <div className="relative z-[2] flex flex-col items-center gap-[20px]">
+          <Image
+            className="mt-[14px]"
+            priority
+            src={LoginImage}
+            alt="로그인"
+            width={269}
+            height={199}
+          />
+          <span className="text-sm text-white">
+            문장을 기록해서 <span className="font-semibold">리츄얼을 형성</span>해보세요.
+          </span>
+          <button className="h-[60px] w-full rounded-[10px] bg-[#fee500] font-bold">
+            카카오로 로그인하기
+          </button>
+        </div>
+      </Container>
+
+      <Image
+        className="fixed bottom-0 w-full min-w-[320px]"
+        priority
+        src={LoginBackgroundImage}
+        alt="로그인 배경"
+        height={358}
+      />
     </>
   );
 };
