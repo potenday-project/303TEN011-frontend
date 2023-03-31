905c8f8e384c122752e1647b1a170d0a3e1ed571
devyouth94 <devyouth94@gmail.com>
2023-04-01 01:05:19 +0900

    Feat: 아카이브 상세 페이지 기본 구현

--- src/pages/archive/[id].tsx
+++ src/pages/archive/[id].tsx
@@ -1,12 +1,27 @@
+import Container from "@/components/common/Container";
+import InnerContainer from "@/components/common/InnerContainer";
+import PageTitle from "@/components/common/PageTitle";
+import TextButton from "@/components/common/TextButton";
 import { useRouter } from "next/router";
 
 const Detail = () => {
-  const { query } = useRouter();
+  const { back } = useRouter();
+
+  const handleClickClose = () => {
+    back();
+  };
 
   return (
-    <>
-      <h1>상세페이지: {query.id}</h1>
-    </>
+    <Container bgColor="bg-main-900" className="flex items-end">
+      <InnerContainer className="h-inner mt-[50px]">
+        <header className="flex h-[70px] items-center justify-between px-6">
+          <PageTitle title="나의 한줄" />
+          <TextButton text="닫기" onClick={handleClickClose} />
+        </header>
+
+        <div className="bg-red-200 px-6">dd</div>
+      </InnerContainer>
+    </Container>
   );
 };
 
