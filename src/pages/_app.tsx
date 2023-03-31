46f7ca3ff6f8120774efac78fb8d3f7a356116b6
devyouth94 <devyouth94@gmail.com>
2023-04-01 02:55:03 +0900

    Refactor: _app.tsx 파일 수정 및 css 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,34 @@
+import "@/styles/height.css";
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { pretendard } from "@/util/fonts";
 
-// if (typeof window === "undefined") {
-//   (async () => {
-//     const { server } = await import("@/mocks/server");
-//     server.listen();
-//   })();
-// } else {
-//   (async () => {
-//     const { worker } = await import("@/mocks/browser");
-//     worker.start();
-//   })();
-// }
+if (typeof window === "undefined") {
+  (async () => {
+    const { server } = await import("@/mocks/server");
+    server.listen();
+  })();
+} else {
+  (async () => {
+    const { worker } = await import("@/mocks/browser");
+    worker.start();
+  })();
+}
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <main className={`${pretendard.variable} font-pretendard`}>
+        <Component {...pageProps} />
+      </main>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
