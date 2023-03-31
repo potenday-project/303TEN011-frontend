b5d0609d5f902f3da2c22189eab1fe21c792ec4c
devyouth94 <devyouth94@gmail.com>
2023-04-01 01:35:24 +0900

    Refactor: 로그인 페이지 정리

---
+++ src/components/login/KakaoButton.tsx
@@ -0,0 +1,9 @@
+const KakaoButton = () => {
+  return (
+    <button className="h-[60px] w-full rounded-[10px] bg-[#fee500] font-bold">
+      카카오로 로그인하기
+    </button>
+  );
+};
+
+export default KakaoButton;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 import Container from "@/components/common/Container";
 import PageTitle from "@/components/common/PageTitle";
-import Image from "next/image";
+import KakaoButton from "@/components/login/KakaoButton";
+
 import LoginImage from "@/static/images/login.svg";
 import LoginBackgroundImage from "@/static/images/login_background.svg";
 
@@ -10,31 +13,21 @@ const Login = () => {
       <Container className="relative flex flex-col justify-between px-6 py-[30px]">
         <div className="relative z-[2] flex flex-col items-start justify-start gap-[45px]">
           <PageTitle title="하루 한줄" />
-
-          <div className="flex flex-col gap-1 text-2xl font-medium">
-            <div className="flex items-center gap-1">
-              <PageTitle title="오늘의 한줄" fontSize="text-2xl" />
+          <div className="flex flex-col gap-1 text-headline1 font-medium">
+            <div className="flex items-center">
+              <PageTitle title="오늘의 한줄" fontSize="text-headline1" />
               <span>을</span>
             </div>
             <span>기록으로 남기는</span>
           </div>
         </div>
 
-        <div className="relative z-[2] flex flex-col items-center gap-[20px]">
-          <Image
-            className="mt-[14px]"
-            priority
-            src={LoginImage}
-            alt="로그인"
-            width={269}
-            height={199}
-          />
-          <span className="text-sm text-white">
+        <div className="relative z-[2] flex flex-col items-center">
+          <Image priority src={LoginImage} alt="로그인" width={269} height={199} />
+          <span className="mt-10 mb-5 text-body2 text-white">
             문장을 기록해서 <span className="font-semibold">리츄얼을 형성</span>해보세요.
           </span>
-          <button className="h-[60px] w-full rounded-[10px] bg-[#fee500] font-bold">
-            카카오로 로그인하기
-          </button>
+          <KakaoButton />
         </div>
       </Container>
 
