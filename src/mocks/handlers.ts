9e2ecd5dc6352e99477920d2b962ff61e9c721d7
devyouth94 <devyouth94@gmail.com>
2023-04-01 00:50:29 +0900

    Feat: msw 핸들러 설정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,14 @@
 import { rest } from "msw";
 
+const archive: any[] = [];
+
 export const handlers = [
-  rest.get("/login", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        result: { id: 1, userId: "kakao@kakao.com", nickname: "테스트" },
-        message: "Success",
-        ok: true,
-      }),
-    );
+  rest.get("/api/archives", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(archive));
+  }),
+
+  rest.post("/api/archives", (req, res, ctx) => {
+    archive.push(req.body);
+    return res(ctx.status(201));
   }),
 ];
