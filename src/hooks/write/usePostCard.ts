4a3882b8444171331ac97844667bb9097a9a67c8
devyouth94 <devyouth94@gmail.com>
2023-04-01 23:47:15 +0900

    Refactor: 문장 등록 성공시 상세 페이지로 이동

--- src/hooks/write/usePostCard.ts
+++ src/hooks/write/usePostCard.ts
@@ -1,12 +1,18 @@
+import { useRouter } from "next/router";
 import { useMutation } from "@tanstack/react-query";
 
 import { postCard } from "@/apis/api";
 import { useWriteState } from "@/store/useWriteStore";
 
 const usePostCard = () => {
+  const { replace } = useRouter();
   const cardData = useWriteState();
 
-  return useMutation(() => postCard(cardData));
+  return useMutation(() => postCard(cardData), {
+    onSuccess: (data) => {
+      replace(`/archive/${data.archiveId}`);
+    },
+  });
 };
 
 export default usePostCard;
