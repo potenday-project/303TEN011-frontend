1ecaf4027f7420484d38c9097f72e5bd9b3cc61f
devyouth94 <devyouth94@gmail.com>
2023-04-02 12:40:28 +0900

    Refactor: 메타 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <title>하루 한 줄, 오늘의 한줄을 남겨보세요.</title>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width ,user-scalable=no" />
+        <meta property="og:title" content="하루 한 줄" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://one-line-a-day-kappa.vercel.app/" />
+      </Head>
       <body>
         <Main />
         <NextScript />
