c2b9bd6f589e241d4c2d1d16692b215d70d44a67
devyouth94 <devyouth94@gmail.com>
2023-04-02 11:50:26 +0900

    Refactor: 카드 textarea overflow 변경

--- src/components/write/CardContainer.tsx
+++ src/components/write/CardContainer.tsx
@@ -25,7 +25,7 @@ const CardContainer = () => {
           placeholder="오늘의 한줄을 입력해주세요"
           value={content}
           onChange={(event) => postData("content", event.target.value)}
-          className={`w-full resize-none overflow-y-hidden bg-transparent py-1 text-center text-[5vw] leading-[7.5vw] ${
+          className={`scrollbar-hidden w-full resize-none overflow-y-auto bg-transparent py-1 text-center text-[5vw] leading-[7.5vw] ${
             pretendard.variable
           } ${scdream.variable} ${bookk.variable} ${jsongmyung.variable} ${fontStyle} ${
             fontColor === "text-white" ? "placeholder:text-white" : "placeholder:text-black"
