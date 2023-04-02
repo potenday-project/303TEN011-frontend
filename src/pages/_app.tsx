7eb049e4e597a9fbbd3b5c3ed07ee40ab10ee803
devyouth94 <devyouth94@gmail.com>
2023-04-02 13:01:08 +0900

    Fix: Head 위치 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { pretendard } from "@/util/fonts";
+import Head from "next/head";
 
 // if (typeof window === "undefined") {
 //   (async () => {
@@ -24,11 +25,20 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className={`${pretendard.variable} font-pretendard`}>
-        <Component {...pageProps} />
-      </main>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>하루 한 줄, 오늘의 한줄을 남겨보세요.</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width ,user-scalable=no" />
+        <meta property="og:title" content="하루 한 줄" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://one-line-a-day-kappa.vercel.app/" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <main className={`${pretendard.variable} font-pretendard`}>
+          <Component {...pageProps} />
+        </main>
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </QueryClientProvider>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <title>하루 한 줄, 오늘의 한줄을 남겨보세요.</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width ,user-scalable=no" />
-        <meta property="og:title" content="하루 한 줄" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://one-line-a-day-kappa.vercel.app/" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
