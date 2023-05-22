23c94d947c795037e83ef65d2e5d7d7142810d0c
devyouth94 <devyouth94@gmail.com>
2023-05-22 16:31:06 +0900

    Refactor: 폰트 및 이미지 디렉토리 변경

--- src/@shared/utils/fonts.ts
+++ src/@shared/utils/fonts.ts
@@ -1,45 +1,45 @@
 import localFont from "next/font/local";
 
 export const bebas = localFont({
-  src: "../static/fonts/BebasNeue.woff",
+  src: "../../../public/fonts/BebasNeue.woff",
   variable: "--font-bebas",
 });
 
 export const scdream = localFont({
-  src: "../static/fonts/SCDream.woff",
+  src: "../../../public/fonts/SCDream.woff",
   variable: "--font-scdream",
 });
 
 export const bookk = localFont({
-  src: "../static/fonts/Bookk.woff",
+  src: "../../../public/fonts/Bookk.woff",
   variable: "--font-bookk",
 });
 
 export const jsongmyung = localFont({
-  src: "../static/fonts/JSongMyung.woff",
+  src: "../../../public/fonts/JSongMyung.woff",
   variable: "--font-jsongmyung",
 });
 
 export const pretendard = localFont({
   src: [
     {
-      path: "../../public/fonts/Pretendard-Regular.woff",
+      path: "../../../public/fonts/Pretendard-Regular.woff",
       weight: "400",
     },
     {
-      path: "../../public/fonts/Pretendard-Medium.woff",
+      path: "../../../public/fonts/Pretendard-Medium.woff",
       weight: "500",
     },
     {
-      path: "../../public/fonts/Pretendard-SemiBold.woff",
+      path: "../../../public/fonts/Pretendard-SemiBold.woff",
       weight: "600",
     },
     {
-      path: "../../public/fonts/Pretendard-Bold.woff",
+      path: "../../../public/fonts/Pretendard-Bold.woff",
       weight: "700",
     },
     {
-      path: "../../public/fonts/Pretendard-ExtraBold.woff",
+      path: "../../../public/fonts/Pretendard-ExtraBold.woff",
       weight: "800",
     },
   ],
