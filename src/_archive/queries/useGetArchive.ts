288e2c31748fc94e223f4e78c39c846cc69713f1
devyouth94 <devyouth94@gmail.com>
2023-05-25 00:10:26 +0900

    Refactor: api 데이터 수정

--- src/_archive/queries/useGetArchive.ts
+++ src/_archive/queries/useGetArchive.ts
@@ -2,13 +2,24 @@ import { GetCardData, PageableData } from "@/@shared/types/cardTypes";
 import api from "@/@shared/utils/api";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
-const getArchive = async ({ pageParam = 1 }) => {
-  const { data } = await api.get<{ content: GetCardData[]; pageableCustom: PageableData }>(
-    "/api/archives",
-    { params: { page: pageParam } },
-  );
+interface Data {
+  archives: {
+    content: GetCardData[];
+    pageableCustom: PageableData;
+  };
+  totalCreationDate: {
+    [year: number]: number[];
+  };
+}
 
-  return { data: data.content, nextPage: pageParam + 1, isLastPage: data.pageableCustom.last };
+const getArchive = async ({ pageParam = 1 }) => {
+  const { data } = await api.get<Data>("/api/archives", { params: { page: pageParam } });
+
+  return {
+    data: data.archives.content,
+    nextPage: pageParam + 1,
+    isLastPage: data.archives.pageableCustom.last,
+  };
 };
 
 const useGetArchive = () => {
